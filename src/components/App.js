109758832d712bfc6416a553b7c2fde990be2ad2
Razzakh <mrazzakh123@gmail.com>
2024-07-19 04:03:11 +0530

    now2

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,14 @@ const App = () => {
     const v1=e.target.value;
     setval(v1);
   }
-  const handlesearch=()=>{
+  const enter=(e)=>{
+    //e.preventDefault();
+if (e.key=='Enter') {
+  handlesearch(e);
+}
+  }
+  const handlesearch=(e)=>{
+    e.preventDefault();
     console.log('serch',val);
     axios.request(`http://www.omdbapi.com/?t=${val}&apikey=99eb9fd1`).then((res)=>{
       console.log(res.data,res.data.Response);
@@ -30,10 +37,11 @@ const App = () => {
   }
   return (
     <div>
-      this is it
-      <h2>Search Movie</h2>
-      <input value={val} onChange={handleinput} type="text" placeholder="Enter a Movie name" />
-      <button onClick={handlesearch}>Search</button>
+        <h2>Search Movie</h2>
+    <form>
+    <input value={val} onKeyDown={enter} onChange={handleinput} type="text" placeholder="Enter a Movie name" />
+    <button onClick={handlesearch}>Search</button>
+    </form>
      <ul>
      {render && <li> <h2>{data.Title}</h2>
      <img src={data.Poster} alt="Image" /></li>}
